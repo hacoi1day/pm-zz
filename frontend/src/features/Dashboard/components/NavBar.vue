d87b392ddfbb39fca3ae0350c33c5824107301dd
ha_intern <havt.intern@gmail.com>
2021-04-18 00:32:17 +0700

    fix iframe vue

--- frontend/src/features/Dashboard/components/NavBar.vue
+++ frontend/src/features/Dashboard/components/NavBar.vue
@@ -3,9 +3,9 @@
     <b-navbar-brand href="#">PM</b-navbar-brand>
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
+      <!-- <b-navbar-nav>
         <b-nav-item href="#">Link</b-nav-item>
-      </b-navbar-nav>
+      </b-navbar-nav> -->
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
@@ -38,6 +38,9 @@ export default {
   computed: mapState({
     user: state => state.user.userInfo
   }),
+  created () {
+    console.log(this.user);
+  },
   methods: {
     showInfo () {
       this.$router.push({name: 'user-info'});
