d8eb1b418703e53046c03acbbc7f0c21bbfa526d
ha_intern <havt.intern@gmail.com>
2021-04-27 14:45:25 +0700

    fix UI dropdown menu

---
+++ frontend/src/components/menu/MenuDropdown.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="menu">
+    <p class="title" 
+      :class="{'has-child': router.children && router.children.length > 0}"
+      v-if="router.children && router.children.length > 0"
+      @click="toggleDropdown"
+    >
+      <span>{{ router.title }}</span>
+      <font-awesome-icon class="icon" 
+        :class="{'active': isShow}"
+        icon="angle-up"/>
+    </p>
+    <p class="title" v-else><router-link :to="router.link"><span>{{ router.title }}</span></router-link></p>
+    <div class="dropdown" :class="{'active': isShow}" v-if="router.children && router.children.length > 0" v-show="isShow">
+      <ul>
+        <li v-for="child of router.children" :key="child.id">
+          <router-link :to="child.link">{{ child.title }}</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'menu-dropdown',
+  props: {
+    router: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+      isShow: true
+    };
+  },
+  methods: {
+    toggleDropdown () {
+      this.isShow = !this.isShow;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.menu {
+  background-color: white;
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+
+  .title {
+    font-size: 15px;
+    font-weight: bold;
+    margin-left: 5px;
+    margin-bottom: 0px;
+    cursor: pointer;
+    
+    &.has-child {
+      padding-bottom: 5px;
+      border-bottom: 1px solid #e3e3e3;
+    }
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .icon {
+      transition: 0.4s;
+      &.active {
+        transform: rotate(-180deg);
+      }
+    }
+  }
+  .dropdown {
+    transition: 0.4s;
+    height: 0px;
+    overflow: hidden;
+    &.active {
+      height: 100%;
+    }
+    ul {
+      list-style-type: none;
+      margin-bottom: 0;
+      li {
+        padding: 5px 10px;
+        cursor: pointer;
+        &:hover {
+          background-color: lightgrey;
+        }
+        a {
+          text-decoration: none;
+        }
+      }
+    }
+  }
+}
+</style>
--- frontend/src/features/Dashboard/components/LeftBar.vue
+++ frontend/src/features/Dashboard/components/LeftBar.vue
@@ -1,45 +1,55 @@
 <template>
   <div class="left-bar">
-    
-    <div class="menu">
-      <p class="title">Nhân viên</p>
-      <div class="dropdown">
-        <ul>
-          <li><router-link to="/user/list">Danh sách</router-link></li>
-          <li><router-link to="/user/create">Thêm mới</router-link></li>
-        </ul>
-      </div>
-    </div>
-
-    <div class="menu">
-      <p class="title">Phòng ban</p>
-      <div class="dropdown">
-        <ul>
-          <li><router-link to="/department/list">Danh sách</router-link></li>
-          <li><router-link to="/department/create">Thêm mới</router-link></li>
-        </ul>
-      </div>
-    </div>
-
-    <div class="menu">
-      <p class="title"><router-link to="/check-in">Lịch làm việc</router-link></p>
-    </div>
-    
-    <div class="menu">
-      <p class="title">Yêu cầu</p>
-      <div class="dropdown">
-        <ul>
-          <li><router-link to="/request/list">Yêu cầu của tôi</router-link></li>
-          <li><router-link to="/request/create">Tạo yêu cầu</router-link></li>
-        </ul>
-      </div>
-    </div>
+    <menu-dropdown
+      v-for="router of routers"
+      :key="router.id"
+      :router="router"
+    />
   </div>
 </template>
 
 <script>
+import MenuDropdown from '../../../components/menu/MenuDropdown'
 export default {
-
+  name: 'left-bar',
+  components: {
+    'menu-dropdown': MenuDropdown
+  },
+  data () {
+    return {
+      routers: [
+        {
+          id: 100,
+          title: 'Nhân viên',
+          children: [
+            { id: 101, title: 'Danh sách', link: '/user/list' },
+            { id: 102, title: 'Thêm mới', link: '/user/create' },
+          ]
+        },
+        {
+          id: 200,
+          title: 'Phòng ban',
+          children: [
+            { id: 201, title: 'Danh sách', link: '/department/list' },
+            { id: 202, title: 'Thêm mới', link: '/department/create' },
+          ]
+        },
+        {
+          id: 300,
+          title: 'Lịch làm việc',
+          link: '/check-in'
+        },
+        {
+          id: 400,
+          title: 'Yêu cầu',
+          children: [
+            { id: 401, title: 'Yêu cầu của tôi', link: '/request/list' },
+            { id: 402, title: 'Tạo yêu cầu', link: '/request/create' },
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -49,33 +59,5 @@ export default {
   height: calc(100vh - 56px);
   padding: 10px;
   overflow-y: auto;
-}
-.menu {
-  background-color: white;
-  padding: 10px;
-  border-radius: 5px;
-  margin-bottom: 10px;
-
-  .title {
-    font-size: 15px;
-    font-weight: bold;
-    margin-left: 5px;
-    margin-bottom: 0px;
-    padding-bottom: 5px;
-    border-bottom: 1px solid #e3e3e3;
-  }
-  .dropdown {
-    ul {
-      list-style-type: none;
-      margin-bottom: 0;
-      li {
-        padding: 5px 10px;
-        cursor: pointer;
-        &:hover {
-          background-color: lightgrey;
-        }
-      }
-    }
-  }
 }
 </style>
