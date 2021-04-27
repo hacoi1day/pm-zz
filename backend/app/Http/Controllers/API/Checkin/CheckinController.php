d4107184ce0c7f328b12f721c09a402b6a946049
ha_intern <havt.intern@gmail.com>
2021-04-27 13:46:07 +0700

    fix api calendar

--- backend/app/Http/Controllers/API/Checkin/CheckinController.php
+++ backend/app/Http/Controllers/API/Checkin/CheckinController.php
@@ -21,7 +21,11 @@ class CheckinController extends Controller
         try {
             $start_date = date($request->input('start_date'));
             $end_date = date($request->input('end_date'));
-            $result = $this->checkin->whereBetween('date', [$start_date, $end_date])->get();
+            $userId = Auth::guard('api')->id();
+            $result = $this->checkin
+                ->where('user_id', $userId)
+                ->whereBetween('date', [$start_date, $end_date])
+                ->get();
             return response()->json($result, 200);
         } catch(Exception $e) {
             return response()->json([
