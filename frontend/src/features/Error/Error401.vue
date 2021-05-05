86d763d5af99397c865bf4f877b12c12093eeb1b
ha_intern <havt.intern@gmail.com>
2021-05-05 17:39:19 +0700

    role in frontend

---
+++ frontend/src/features/Error/Error401.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-card
+    header="Không có quyền truy cập !!!"
+    header-tag="header"
+  >
+    <h2 class="text-401 text-center">401</h2>
+    <h4 class="text-center">Không có quyền truy cập !!!</h4>
+    <div class="text-center">
+      <b-button variant="success" @click="goHome()">Trang chủ</b-button>
+    </div>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'error-401',
+  methods: {
+    goHome() {
+      this.$router.push({name: 'home'});
+    }
+  }
+}
+</script>
+
+<style>
+.text-401 {
+  font-size: 80px;
+  font-weight: bold;
+  margin: 20px;
+}
+</style>
