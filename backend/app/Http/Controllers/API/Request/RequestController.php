01d2bfa15decca77cba92228df82a75c6f1c8030
ha_intern <havt.intern@gmail.com>
2021-05-06 16:22:42 +0700

    send mail to root and manager

--- backend/app/Http/Controllers/API/Request/RequestController.php
+++ backend/app/Http/Controllers/API/Request/RequestController.php
@@ -4,17 +4,22 @@ namespace App\Http\Controllers\API\Request;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Request\StoreRequest;
+use App\Jobs\Request\SendMail;
 use App\Models\Request;
+use App\Models\User;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 
 class RequestController extends Controller
 {
     private $request;
+    private $user;
 
-    public function __construct(Request $request)
+    public function __construct(Request $request, User $user)
     {
         $this->request = $request;
+        $this->user = $user;
     }
 
     public function myRequest()
@@ -39,11 +44,32 @@ class RequestController extends Controller
     {
         try {
             $params = $request->all();
-            $params['user_id'] = Auth::guard('api')->id();
+
+            $user = Auth::guard('api')->user();
+            $params['user_id'] = $user->id;
             if (!$request->has('status')) {
                 $params['status'] = 1;
             }
             $item = $this->request->create($params);
+
+            $data = [
+                'email' => $user->email,
+                'name' => $user->name,
+                'project' => $item->project,
+                'cause' => $item->cause
+            ];
+
+            // send mail to root account
+            $root = $this->user->where('role_id', 1)->first();
+            if ($root) {
+                SendMail::dispatch($root->email, $data)->delay(Carbon::now());
+            }
+            // send mail to manager account
+            $manager = $user->department->manager;
+            if ($root) {
+                SendMail::dispatch($manager->email, $data)->delay(Carbon::now());
+            }
+
             return response()->json($item, 201);
         } catch (Exception $e) {
             return response()->json([
