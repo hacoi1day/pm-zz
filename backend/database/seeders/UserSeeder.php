ed3769779b04ea290a625a26f1cc303d1376898d
ha_intern <havt.intern@gmail.com>
2021-05-11 16:03:03 +0700

    seeder

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -21,5 +21,43 @@ class UserSeeder extends Seeder
             'password' => bcrypt('123456'),
             'role_id' => 1
         ]);
+        User::create([
+            'id' => 2,
+            'name' => 'Quản lý 1',
+            'email' => 'manager1@gmail.com',
+            'password' => bcrypt('123456'),
+            'role_id' => 2
+        ]);
+        User::create([
+            'id' => 3,
+            'name' => 'Quản lý 2',
+            'email' => 'manager2@gmail.com',
+            'password' => bcrypt('123456'),
+            'role_id' => 2
+        ]);
+        User::create([
+            'id' => 4,
+            'name' => 'Nhân viên 1',
+            'email' => 'user1@gmail.com',
+            'password' => bcrypt('123456'),
+            'role_id' => 3,
+            'department_id' => 1
+        ]);
+        User::create([
+            'id' => 5,
+            'name' => 'Nhân viên 2',
+            'email' => 'user2@gmail.com',
+            'password' => bcrypt('123456'),
+            'role_id' => 3,
+            'department_id' => 1
+        ]);
+        User::create([
+            'id' => 6,
+            'name' => 'Nhân viên 3',
+            'email' => 'user3@gmail.com',
+            'password' => bcrypt('123456'),
+            'role_id' => 3,
+            'department_id' => 2
+        ]);
     }
 }
