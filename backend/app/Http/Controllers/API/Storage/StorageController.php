aa5ecb884694ec66c96037fa43d5004bf1b4e60d
ha_intern <havt.intern@gmail.com>
2021-05-13 11:20:01 +0700

    Store Service

--- backend/app/Http/Controllers/API/Storage/StorageController.php
+++ backend/app/Http/Controllers/API/Storage/StorageController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers\API\Storage;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Storage\StoreRequest;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
+use App\Services\StorageService;
 
 class StorageController extends Controller
 {
+    private $storageService;
+
+    public function __construct(StorageService $storageService) {
+        $this->storageService = $storageService;
+    }
     public function storeFile(StoreRequest $request)
     {
         $file = $request->file('file');
-        $fileName = 'images/'.time().'_'.$file->getClientOriginalName();
-        Storage::disk('public')->put($fileName, file_get_contents($file));
-
+        $fileName = $this->storageService->uploadFile($file);
         return response()->json([
             'url' => env('APP_URL').'storage/'.$fileName
         ], 200);
