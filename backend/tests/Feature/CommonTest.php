101015901bb0f7aca0fbbd028788dd498d06ab87
ha_intern <havt.intern@gmail.com>
2021-05-14 14:39:34 +0700

    Common Test

---
+++ backend/tests/Feature/CommonTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CommonTest extends TestCase
+{
+    private static $token;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $response = $this->post('api/v1/login', [
+            'email' => 'admin@gmail.com',
+            'password' => '123456'
+        ]);
+        $user = json_decode($response->getContent());
+        self::$token = $user->token;
+    }
+
+    public function test_check_unique_true()
+    {
+        $response = $this
+            ->withHeader('Authorization', 'Bearer '.self::$token)
+            ->post('api/v1/common/check-unique/users/email/1', [
+                'value' => 'admin@gmail.com'
+            ]);
+        $response->assertStatus(200)->assertSeeText('1');
+    }
+
+    public function test_check_unique_false()
+    {
+        $response = $this
+            ->withHeader('Authorization', 'Bearer '.self::$token)
+            ->post('api/v1/common/check-unique/users/email/2', [
+                'value' => 'admin@gmail.com'
+            ]);
+            $response->assertStatus(200)->assertSeeText('0');
+    }
+}
