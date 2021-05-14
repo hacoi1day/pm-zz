9d44f3f66d2b52191e998dd4a15fc4641178448c
ha_intern <havt.intern@gmail.com>
2021-05-14 13:58:53 +0700

    User Test

---
+++ backend/tests/Feature/UserTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    private static $token;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $response = $this->post('api/v1/login', [
+            'email' => 'admin@gmail.com',
+            'password' => '123456'
+        ]);
+        $user = json_decode($response->getContent());
+        self::$token = $user->token;
+    }
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_index_user()
+    {
+        $response = $this
+            ->withHeader('Authorization', 'Bearer '.self::$token)
+            ->get('api/v1/user/user');
+        $response->assertStatus(200)->assertJsonStructure(['current_page', 'data', 'total']);
+    }
+
+    public function test_show_user()
+    {
+        $response = $this
+            ->withHeader('Authorization', 'Bearer '.self::$token)
+            ->get('api/v1/user/user/1');
+        $response->assertStatus(200)->assertJsonStructure(['name']);
+    }
+
+    public function test_store_user()
+    {
+        $response = $this
+            ->withHeader('Authorization', 'Bearer '.self::$token)
+            ->post('api/v1/user/user', [
+                'name' => 'Test',
+                'email' => Str::random(6).'@gmail.com',
+                'role_id' => 3,
+
+            ]);
+        $response->assertStatus(201)->assertJsonStructure(['name', 'email']);
+    }
+
+    public function test_update_user()
+    {
+        $response = $this
+            ->withHeader('Authorization', 'Bearer '.self::$token)
+            ->put('api/v1/user/user/1', [
+                'name' => 'Root 1',
+            ]);
+        $response->assertStatus(202)->assertJsonStructure(['name', 'email']);
+    }
+
+    public function test_destroy_user()
+    {
+        $response = $this
+            ->withHeader('Authorization', 'Bearer '.self::$token)
+            ->delete('api/v1/user/user/17');
+
+        if ($response->getStatusCode() === 404) {
+            $response->assertStatus(404)->assertJsonStructure(['status', 'message']);
+        } else {
+            $response->assertStatus(200);
+        }
+    }
+}
