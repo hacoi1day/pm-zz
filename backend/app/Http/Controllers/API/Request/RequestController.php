3de5b3ff13771d35eb4f07bff367c3e8cd7291e1
ha_intern <havt.intern@gmail.com>
2021-06-16 10:49:13 +0700

    Request API Docs

--- backend/app/Http/Controllers/API/Request/RequestController.php
+++ backend/app/Http/Controllers/API/Request/RequestController.php
@@ -21,12 +21,40 @@ class RequestController extends Controller
         $this->requestService = $requestService;
     }
 
+    /**
+     * @OA\Get(
+     *      path="/my-request",
+     *      operationId="getMyRequest",
+     *      tags={"Request"},
+     *      summary="My Request",
+     *      description="My Request",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Paginate My Request",
+     *          @OA\JsonContent()
+     *       )
+     *     )
+     */
     public function myRequest()
     {
         $paginate = $this->requestService->myRequest();
         return response()->json($paginate, 200);
     }
 
+    /**
+     * @OA\Post(
+     *      path="/create-my-request",
+     *      operationId="postCreateMyRequest",
+     *      tags={"Request"},
+     *      summary="Create My Request",
+     *      description="Create My Request",
+     *      @OA\Response(
+     *          response=201,
+     *          description="Create My Request",
+     *          @OA\JsonContent()
+     *       )
+     *     )
+     */
     public function createMyRequest(StoreRequest $request)
     {
         $params = $request->all();
